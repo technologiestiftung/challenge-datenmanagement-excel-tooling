b31e21716bb9c1fe14f940e4cf1af2f9781d2004
ff6347 <hash@ff6347.email>
2023-10-20 14:53:28 +0200

    fix(eslint): Make it happy

--- add-in/et/src/commands/commands.ts
+++ add-in/et/src/commands/commands.ts
@@ -1,9 +1,10 @@
+/* eslint-disable indent */
 /*
  * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
  * See LICENSE in the project root for license information.
  */
 
-/* global global, Office, self, window */
+/* global global, Office*/
 
 Office.onReady(() => {
 	// If needed, Office.js is ready to be called
